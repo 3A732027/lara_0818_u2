0cc51e4e3b7f67ec6c6e56644aef82ab44acb05c
Pai Yen Chang <ken1100327@gmail.com>
2020-10-28 21:41:57 +0800

    練習1.5 改用 HomeController回傳

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('home', function () {
-    return view('home.index');
-});
+Route::get('home', [HomeController::class, 'index']);
